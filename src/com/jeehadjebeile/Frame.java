0e5082a7726706b29938f13f077b36ca1a14d4c9
Jeehad Jebeile <jeehad.jebeile@gmail.com>
2019-08-21 18:13:49 +1000

    Initial Data Structure design for Frame.

--- src/com/jeehadjebeile/Frame.java
+++ src/com/jeehadjebeile/Frame.java
@@ -1,4 +1,50 @@
 package com.jeehadjebeile;
 
 public class Frame {
+
+    private int num;
+    private int bowl1;
+    private int bowl2;
+    private int bowl3;
+    private int score;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public int getBowl1() {
+        return bowl1;
+    }
+
+    public void setBowl1(int bowl1) {
+        this.bowl1 = bowl1;
+    }
+
+    public int getBowl2() {
+        return bowl2;
+    }
+
+    public void setBowl2(int bowl2) {
+        this.bowl2 = bowl2;
+    }
+
+    public int getBowl3() {
+        return bowl3;
+    }
+
+    public void setBowl3(int bowl3) {
+        this.bowl3 = bowl3;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
