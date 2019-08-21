3dbf399d80f72a39204f33d4c935cf81e3e76955
Jeehad Jebeile <jeehad.jebeile@gmail.com>
2019-08-21 18:33:28 +1000

    scoreFrame() function created with basic test cases (which are designed to fail).

--- src/com/jeehadjebeile/Scorecard.java
+++ src/com/jeehadjebeile/Scorecard.java
@@ -2,7 +2,27 @@ package com.jeehadjebeile;
 
 public class Scorecard {
 
-    public static void main(String[] args) {
-	// write your code here
+    private Frame [] frames = new Frame[10];
+
+    public Frame[] getFrames() {
+        return frames;
+    }
+
+    public static Scorecard getInstance() {
+        return new Scorecard();
+    }
+
+    /**
+     * Given a scorecard and the results of two bowls, return an updated scorecard.
+     */
+    public static Scorecard scoreFrame(Scorecard scorecard, int bowl1, int bowl2) {
+        return null;
+    }
+
+    /**
+     * Returns the current score based on the number of frames/bowls played.
+     */
+    public int getCurrentScore() {
+        return 0;
     }
 }
--- src/com/jeehadjebeile/ScorecardTest.java
+++ src/com/jeehadjebeile/ScorecardTest.java
@@ -4,4 +4,18 @@ import junit.framework.TestCase;
 
 public class ScorecardTest extends TestCase {
 
+    public void testScoreFrame() {
+
+        Scorecard scorecard = Scorecard.getInstance();
+        int bowl1 = 4;
+        int bowl2 = 5;
+
+        scorecard = Scorecard.scoreFrame(scorecard, bowl1, bowl2);
+
+        assertNotNull(scorecard);
+
+        assertEquals(9, scorecard.getCurrentScore());
+
+    }
+
 }
